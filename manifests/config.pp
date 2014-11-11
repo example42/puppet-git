f677332a834f8626f2a64a41dca3e8cc878cf73a
Americas <prplopes@gmail.com>
2014-11-11 18:52:00 +0000

    w/out testing

--- manifests/config.pp
+++ manifests/config.pp
@@ -56,8 +56,8 @@ define git::config(
 
    exec { $real_section_key:
       path => "/bin",
-      command => "${real_command} ${real_section_key} \"$value\"",
-      onlyif  => "${real_command} ${real_section_key} | grep -c '${value}'",
+      command => "${real_command} ${real_section_key} \"${value}\"",
+      #onlyif  => "${real_command} ${real_section_key} | grep -c '${value}'",
       require => Package['git'],
    }
 }
