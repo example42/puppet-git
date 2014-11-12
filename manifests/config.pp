1e052986fb6c1e5e4647c6608e7198f0a5c7f109
Americas <prplopes@gmail.com>
2014-11-12 10:12:36 +0000

    no path

--- manifests/config.pp
+++ manifests/config.pp
@@ -55,7 +55,6 @@ define git::config(
    }
 
    exec { $real_section_key:
-      path => "/bin",
       command => "${real_command} ${real_section_key} \"${value}\"",
       #onlyif  => "${real_command} ${real_section_key} | grep -c '${value}'",
       require => Package['git'],
